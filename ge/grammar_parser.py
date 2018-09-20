b8db0e9052f5723a13eea7381f3e7ac1c54329c8
rhrlima <ricardo_des@hotmail.com>
2018-09-20 18:47:19 -0300

    Added grammar parser

---
+++ ge/grammar_parser.py
@@ -0,0 +1,62 @@
+import numpy as np
+import re
+import sys
+
+MAX_LOOPS = 1000
+grammar = None
+
+def load_grammar(file):
+	lines = []
+	with open(grammar_file, 'r') as gf:
+		for line in gf:
+			line = re.sub('\\s+|\n', '', line) #remove spaces and '\n'
+			if re.match('<[a-z_]+>::=', line) == None:
+				lines[len(lines)-1] += line
+			else:
+				lines.append(line)
+
+	global grammar
+	grammar = {'<start>': None}
+	for line in lines:
+		rule, prod = line.split('::=')
+		grammar[rule] = prod.split('|')
+		if grammar['<start>'] == None: grammar['<start>'] = rule
+	print(grammar)
+
+
+def parse(ind):
+	index = 0
+	loop_count = 0
+	match= 0
+
+	prod = grammar['<start>']
+	print('start', prod)
+	while match != None:
+		match= re.search('<[a-z_]+>', prod)
+		#print('match', match)
+		if match != None:
+			token = match.group(0)
+			#print('token', token)
+			repl = ind[index] % len(grammar[token])
+			#print('index', index, 'ind', ind[index], 'size', len(grammar[token]))
+			prod = prod.replace(token, grammar[token][repl], 1)
+			print('{} --> {}'.format(token, grammar[token][repl]))
+			index += 1
+			if index >= len(ind): index = 0
+		print(prod)
+		loop_count += 1
+		if loop_count >= MAX_LOOPS:
+			print('invalid')
+			return None
+	return prod
+
+
+if __name__ == '__main__':
+	grammar_file = sys.argv[1]
+	
+	rand = np.random
+	ind = rand.randint(0, 255, rand.randint(1, 10))
+		
+	load_grammar(grammar_file)
+	phen = parse(ind)
+	print(phen)
--- keras/keras-mnist.py
+++ keras/keras-mnist.py
@@ -6,10 +6,12 @@ from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling
 from keras.models import Sequential, model_from_json
 from keras.utils import np_utils
 
-from matplotlib import pyplot as plt
-
 K.set_image_dim_ordering('th')
 np.random.seed(123)
+
+#--------------------------------------------------------
+
+print('Loading dataset')
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 print('train shape', X_train.shape, y_train.shape)
@@ -25,9 +27,13 @@ X_test /= 255
 Y_train = np_utils.to_categorical(y_train, 10)
 Y_test = np_utils.to_categorical(y_test, 10)
 
+#--------------------------------------------------------
+
+print('Building model')
+
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), activation='relu', data_format='channels_first'))
+model.add(Conv2D(32, (3, 3), activation='relu', data_format='channels_first', input_shape=(1, 28, 28)))
 model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -37,27 +43,26 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
+model.summary()
+
+#--------------------------------------------------------
+
+print('Running')
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train, Y_train, batch_size=128, epochs=1, verbose=1)
+# print('Train')
+# model.fit(X_train, Y_train, batch_size=128, epochs=1, verbose=1)
 
+# print('Test')
 score = model.evaluate(X_test, Y_test, verbose=0)
-print('score', score)
+print('Score -> loss: {}\taccuracy: {}'.format(score[0], score[1]))
 
-model_json = model.to_json()
-with open('model.json', 'w') as json_file:
-	json_file.write(model_json)
-model.save_weights('model.h5')
-print('Model saved to disk')
+# #--------------------------------------------------------
 
-print('------')
+# print('Saving model to file')
 
-with open('model.json', 'r') as json_file:
-	model_json = json_file.read()
-model2 = model_from_json(model_json)
-model2.load_weights('model.h5')
-print('Loaded model from disk')
-
-model2.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-score = model.evaluate(X_test, Y_test, verbose=0)
-print('score2', score)
+# model_json = model.to_json()
+# with open('model.json', 'w') as json_file:
+# 	json_file.write(model_json)
+# model.save_weights('model.h5')
