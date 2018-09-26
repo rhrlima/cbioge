5c0a7e5532eb0cbf90498aa927dd83e7ca9fc069
rhrlima <ricardo_des@hotmail.com>
2018-09-26 04:20:23 +0000

    implementing parser

--- ge/cnn_design.py
+++ ge/cnn_design.py
@@ -1,53 +1,81 @@
+import json
 import numpy as np
+import sys
 
 from keras import layers
 from keras.models import Sequential
 
 import grammar_parser as parser
 
+INPUT_SHAPE = (1, 28, 28)
+INPUT_DIM = len(INPUT_SHAPE)-1
 OUTPUT_SHAPE = 10
+
 
 class Solution:
 
 	genotype = None
 	phenotype = None
 
-	def __init__(self):
-		self.genotype = rand.randint(0, 255, rand.randint(1, 10))
+	def __init__(self, genes):
+		self.genotype = rand.randint(0, 255, rand.randint(1, 10)) \
+		if not genes else genes
 
 
-def create_solution():
+def create_solution(genes=None):
 
-	return Solution()
+	return Solution(genes)
 
 
 def evaluate(solution):
 	pass
 
 
-def build_from_genotype(solution):
+def build_from_genotype(solution): #['Conv', '64', '(3,3)', 'Dense', '32']
 
+	print('genotype')
 	print(solution.genotype)
-	temp = parser.parse(solution.genotype)
-	pass
+	
+	deriv = parser.parse(solution.genotype)
+	
+	print('phenotype')
+	print(deriv)
+
+	nodes = []
+	node = {'config': {}}
+
+	print('parsing')
+	index = 0
+	while index < len(deriv):
+
+		key, value = deriv[index:index+2]
+		
+		if key == 'class_name':
+
+			if key in node:
+				nodes.append(node)
+				node = {'config': {}}
+
+			print('{}: {}'.format(key, value))
+			node[key] = value
+		else:
+			print('\t{}: {}'.format(key, value))
+			node['config'][key] = eval(value)
+
+		index += 2
+	else:
+		nodes.append(node)
+
+	print('building')
+	for n in nodes:
+		print(n)
+		print(layers.deserialize(n))
 
 
-import json
 if __name__ == '__main__':
 	
 	rand = np.random
-	parser.load_grammar('cnn2.bnf')
+	parser.load_grammar(sys.argv[1])
 
-	solution = create_solution()
+	solution = create_solution([92, 176, 129, 232])
 	build_from_genotype(solution)
-
-	config = {
-		'class_name': 'Dense', 
-		'config': {
-			'name': 'dense_0', 
-			'units': 10, 
-		}
-	}
-
-	d = layers.deserialize(config)
-	print(d.get_config())
