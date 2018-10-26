2d82e338eab023c16e3e71a4b9578e5b96f43ab8
rhrlima <ricardo_des@hotmail.com>
2018-10-26 17:14:32 -0300

    fixed replace bug

--- examples/ge_mnist_example.py
+++ examples/ge_mnist_example.py
@@ -5,6 +5,10 @@ from algorithms import ge
 from grammars import grammar
 from problems import problem
 
+DEBUG = False
+problem.DEBUG = False
+ge.DEBUG = False
+
 # dataset and grammar
 pickle_file = '../datasets/mnist/mnist.pickle'
 grammar_file = '../grammars/cnn.bnf'
@@ -13,22 +17,23 @@ grammar_file = '../grammars/cnn.bnf'
 grammar.load_grammar(grammar_file)
 
 # reading dataset
-mnist_problem = problem.CnnProblem()
-mnist_problem.load_dataset_from_pickle(pickle_file) 
-ge.problem = mnist_problem
+my_problem = problem.CnnProblem()
+my_problem.load_dataset_from_pickle(pickle_file)
+ge.problem = my_problem
 
 # problem parameters
-mnist_problem.batch_size = 128
-mnist_problem.epochs = 1
+my_problem.batch_size = 128
+my_problem.epochs = 1
 
 # changing GE default parameters
-ge.SEED = 42
-ge.POP_SIZE = 2
-ge.MAX_EVALS = 2
-
-print(mnist_problem.input_shape)
+#ge.SEED = 42
+ge.POP_SIZE = 4
+ge.MAX_EVALS = 10
 
 print('--config--')
+print('DATASET', pickle_file)
+print('GRAMMAR', grammar_file)
+
 print('SEED', ge.SEED)
 print('POP', ge.POP_SIZE)
 print('EVALS', ge.MAX_EVALS)
@@ -38,15 +43,15 @@ print('PRUN', ge.PRUN_RATE)
 print('DUPL', ge.DUPL_RATE)
 
 print('--running--')
-best = ge.execute(verbose=True)
+best = ge.execute()
 
 print('--best solution--')
-print(best, best.data['loss'], best.data['acc'])
+print(best, best.fitness)
 best.phenotype.summary()
 
 print('--testing--')
 score = best.phenotype.evaluate(
-	mnist_problem.x_test, 
-	mnist_problem.y_test, 
-	verbose=1)
+	my_problem.x_test, 
+	my_problem.y_test, 
+	verbose=0)
 print('loss: {}\taccuracy: {}'.format(score[0], score[1]))
