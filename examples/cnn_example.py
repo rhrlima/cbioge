9d71c26033da0da1f08fb957499450d2680bf53f
rhrlima <ricardo_des@hotmail.com>
2018-10-31 17:53:49 -0300

    writing relatory

--- examples/cnn_example.py
+++ examples/cnn_example.py
@@ -40,9 +40,10 @@ y_test  = np_utils.to_categorical(y_test , num_classes)
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), 
-	activation='relu', 
-	input_shape=input_shape))
+model.add(Conv2D(filters=32, 
+				 kernel_size=(3, 3), 
+				 activation='relu', 
+				 input_shape=input_shape))
 model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
