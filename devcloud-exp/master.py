6597e818a522ed4f6376098fe4c05b1b83a8e8d0
rhrlima <ricardo_des@hotmail.com>
2018-11-05 17:42:27 -0200

    qsub test fix

--- devcloud-exp/master.py
+++ devcloud-exp/master.py
@@ -9,7 +9,12 @@ for i in range(5):
 	query = ['qsub', 'slave-job', 
 		'-N', 'slave-{0}'.format(i), 
 		'-o', 'slave-{0}.out'.format(i), 
-		'-F' '\'slave.py\'', str(i)]
+		'-F', '\'slave.py\'', str(i)]
+
+	#query = ['python', 'slave.py', str(i)] #test
+	query = ' '.join(query)
+
+	print('command: ', query)
 	p = subprocess.Popen(query, 
 		stdout=subprocess.PIPE, 
 		shell=True)
