d35c1b556f3d9e8163f39ece1da1acc79a90b7bc
rhrlima <ricardo_des@hotmail.com>
2018-11-13 16:09:16 -0200

    test checkpoint

--- algorithms/pge.py
+++ algorithms/pge.py
@@ -8,6 +8,7 @@ from utils import checkpoint
 import glob
 import numpy as np
 import random
+import re
 import time
 
 
@@ -183,8 +184,10 @@ def execute(checkpoint=False):
 	'''
 	'''
 	if checkpoint:
+		print('starting from checkpoint')
 		population, evals = load_state()
 	else:
+		print('starting from zero')
 		population = create_population(POP_SIZE)
 		evaluate_population(population)
 		population.sort(key=lambda x:x.fitness, reverse=not MINIMIZE)
@@ -264,21 +267,10 @@ def save_state(evals, population):
 		'evals': evals
 	}
 
-	# files = ['pop.ckpt', 'args.ckpt']
-	# for file in files:
-	# 	if os.path.exists(file):
-	# 		print('renaming "{0}" to "{0}.old"'.format(file))
-	# 		os.rename(file, file+'.old')
-
-	folder = 'checkpoints/'
+	folder = checkpoint.ckpt_folder
 	if not os.path.exists(folder): os.mkdir(folder)
 	checkpoint.save_args(args, folder+'args_{}.ckpt'.format(evals))
 	checkpoint.save_population(population, folder+'pop_{}.ckpt'.format(evals))
-
-	# for file in files:
-	# 	if os.path.exists(file):
-	# 		print('removing "{0}.old" file'.format(file))
-	# 		os.remove(file+'.old')
 
 
 def load_state(args_file=None, pop_file=None):
@@ -288,51 +280,54 @@ def load_state(args_file=None, pop_file=None):
 	'''
 	global MAX_EVALS, CROSS_RATE, MUT_RATE, PRUN_RATE, DUPL_RATE, MINIMIZE
 
-	folder = 'checkpoints/'
+	folder = checkpoint.ckpt_folder
+
 	pop_files = glob.glob(folder+'pop_*')
+	for i, file in enumerate(pop_files):
+		m = re.match('\\S+_([\\d]+).ckpt', file)
+		id = int(m.group(1)) if m else 0
+		pop_files[i] = {'id': id, 'file': file}
+
 	arg_files = glob.glob(folder+'args_*')
+	for i, file in enumerate(arg_files):
+		m = re.match('\\S+_([\\d]+).ckpt', file)
+		id = int(m.group(1)) if m else 0
+		arg_files[i] = {'id': id, 'file': file}
 
-	if len(pop_files) > 0:
-		pop_files.sort(reverse=True)
-		print(pop_files)
-		pop_file = pop_files[0]
-		population = checkpoint.load_population(pop_file)
+	pop_files.sort(key=lambda x: x['id'], reverse=True)
+	pop_file = pop_files[0]['file']
+	population = checkpoint.load_population(pop_file)
 
-	if len(arg_files) > 0:
-		arg_files.sort(reverse=True)
-		args_file = arg_files[0]
-		args = checkpoint.load_args(args_file)
+	arg_files.sort(key=lambda x: x['id'], reverse=True)
+	args_file = arg_files[0]['file']
+	args = checkpoint.load_args(args_file)
 
-		#POP_SIZE = args['POP_SIZE'] 
-		#args['MIN_GENES']
-		#args['MAX_GENES']
-		
-		#args['MAX_PROCESSES']
-		
-		print('CROSS_RATE set to', CROSS_RATE)
-		CROSS_RATE = args['CROSS_RATE']
-		
-		print('MUT_RATE set to', MUT_RATE)
-		MUT_RATE = args['MUT_RATE']
+	print('[last checkpoint]\npop: {}\nargs: {}'.format(pop_file, args_file))
 
-		print('PRUN_RATE set to', PRUN_RATE)
-		PRUN_RATE = args['PRUN_RATE'] 
+	#POP_SIZE = args['POP_SIZE'] 
+	#args['MIN_GENES']
+	#args['MAX_GENES']
+	#args['MAX_PROCESSES']
+	
+	print('CROSS_RATE set to', CROSS_RATE)
+	CROSS_RATE = args['CROSS_RATE']
+	
+	print('MUT_RATE set to', MUT_RATE)
+	MUT_RATE = args['MUT_RATE']
 
-		print('DUPL_RATE set to', DUPL_RATE)
-		DUPL_RATE = args['DUPL_RATE']
+	print('PRUN_RATE set to', PRUN_RATE)
+	PRUN_RATE = args['PRUN_RATE'] 
 
-		print('MINIZE set to', MINIMIZE)
-		MINIMIZE = args['MINIMIZE']
+	print('DUPL_RATE set to', DUPL_RATE)
+	DUPL_RATE = args['DUPL_RATE']
 
-		evals = args['evals']
-		print('evals set to', evals)
+	print('MINIZE set to', MINIMIZE)
+	MINIMIZE = args['MINIMIZE']
 
-		MAX_EVALS = args['MAX_EVALS'] * 2
-		print('MAX_EVALS set to', MAX_EVALS)
+	evals = args['evals']
+	print('evals set to', evals)
+
+	MAX_EVALS = args['MAX_EVALS']
+	print('MAX_EVALS set to', MAX_EVALS)
 
 	return population, evals
-
-
-	print('last checkpoint\npop:{}\nargs:{}'.format(pop_files[0], arg_files[0]))
-
-	return population
