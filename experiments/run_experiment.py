56e549da096ae9882124a1f6fe68df404bab43a5
rhrlima <ricardo_des@hotmail.com>
2019-02-08 17:53:07 -0200

    cleaning

--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -15,40 +15,37 @@ from problems import CnnProblem
 from utils import checkpoint
 from keras.models import model_from_json
 
+
 def get_arg_parsersed():
 
-	parser = argparse.ArgumentParser(
-		prog='script.py', 
-		description='run experiment')
+	parser = argparse.ArgumentParser(prog='script.py', description='run experiment')
 
-	parser.add_argument('grammar', 
-		#dest='grammar_file', 
-		type=str, 
-		help='grammar file in bnf format')
+	parser.add_argument('grammar', type=str, help='grammar file in bnf format')
+	parser.add_argument('dataset', type=str, help='dataset file in pickle format')
+	
+	parser.add_argument('-f', '--folder', dest='folder', default='checkpoints')
+	parser.add_argument('-c', '--checkpoint', default=True, type=str2bool)
 
-	parser.add_argument('dataset', 
-		#dest='dataset', 
-		type=str, 
-		help='dataset file in pickle format')
+	parser.add_argument('-sd', '--seed', default=None, type=int)
 
-	parser.add_argument('-e', '--evals', 
-		#dest='evals', 
-		default=20, 
-		type=int, 
-		help='number of max evaluations')
+	parser.add_argument('-ep', '--epochs', default=5, type=int)
+	parser.add_argument('-b', '--batch', default=128, type=int)
+	parser.add_argument('-v', '--verbose', default=0, type=int)
 
-	parser.add_argument('-f', '--folder', 
-		dest='folder', 
-		default='checkpoints', 
-		help='folder where checkpoints will be saved')
+	parser.add_argument('-p', '--population', default=True, type=str2bool)
+	parser.add_argument('-e', '--evals', default=20, type=int)
 
-	parser.add_argument('-c', '--checkpoint', 
-		#dest='checkpoint', 
-		default=True, 
-		type=str2bool, 
-		help='indication whether the experiment should continue from checkpoint')
+	parser.add_argument('-cr', '--crossover', default=0.8, type=float)
+	parser.add_argument('-mt', '--mutation', default=0.1, type=float)
+	parser.add_argument('-pr', '--prune', default=0.1, type=float)
+	parser.add_argument('-dp', '--duplication', default=0.1, type=float)
+	parser.add_argument('-min', '--mingenes', default=2, type=int)
+	parser.add_argument('-max', '--maxgenes', default=10, type=int)
+
+	parser.add_argument('-mp', '--maxprocesses', default=8, type=int)
 
 	return parser.parse_args()
+
 
 def str2bool(value):
 	if value.lower() in ('true', 't', '1'):
@@ -57,6 +54,7 @@ def str2bool(value):
 		return False
 	else:
 		raise argparse.ArgumentTypeError('Boolean value expected.')
+
 
 if __name__ == '__main__':
 
@@ -68,21 +66,26 @@ if __name__ == '__main__':
 
 	# problem dataset and parameters
 	problem = CnnProblem(parser, args.dataset)
-	problem.batch_size = 128
-	problem.epochs = 50
-
-	print('teste', problem)
+	problem.batch_size = args.batch
+	problem.epochs = args.epochs
 
 	# checkpoint folder
 	checkpoint.ckpt_folder = args.folder
 
 	# changing pge default parameters
 	pge.problem = problem
-	pge.POP_SIZE = 20
-	pge.MAX_EVALS = int(args.evals)
-	pge.MAX_PROCESSES = 10
-	pge.MIN_GENES = 6
-	pge.MAX_GENES = 12
+	
+	pge.SEED 		= args.seed
+	pge.POP_SIZE 	= args.population
+	pge.MAX_EVALS 	= args.evals
+	pge.CROSS_RATE 	= args.crossover
+	pge.MUT_RATE 	= args.mutation
+	pge.PRUN_RATE 	= args.prune
+	pge.DUPL_RATE 	= args.duplication
+	pge.MIN_GENES 	= args.mingenes
+	pge.MAX_GENES 	= args.maxgenes
+
+	pge.MAX_PROCESSES = args.maxprocesses
 
 	print('--config--')
 	print('DATASET', args.dataset)
@@ -92,10 +95,7 @@ if __name__ == '__main__':
 	print('SEED', pge.SEED)
 	print('POP', pge.POP_SIZE)
 	print('EVALS', pge.MAX_EVALS)
-	print('CROSS', pge.CROSS_RATE)
-	print('MUT', pge.MUT_RATE)
-	print('PRUN', pge.PRUN_RATE)
-	print('DUPL', pge.DUPL_RATE)
+	print(f'CROSS({pge.CROSS_RATE}) MUT({pge.MUT_RATE}) PRUN({pge.PRUN_RATE}) DUPL({pge.DUPL_RATE})')
 
 	print('--running--')
 	best = pge.execute(checkpoint=args.checkpoint)
@@ -112,10 +112,10 @@ if __name__ == '__main__':
 
 		print('--training--')
 		hist = model.fit(problem.x_train, problem.y_train, 
-			batch_size=128, epochs=50, verbose=0)
+			batch_size=128, epochs=50, verbose=args.verbose)
 		print('loss: {}\taccuracy: {}'.format(
 			np.mean(hist.history['loss']), np.mean(hist.history['acc'])))
 
 		print('--testing--')
-		score = model.evaluate(problem.x_test, problem.y_test, verbose=0)
+		score = model.evaluate(problem.x_test, problem.y_test, verbose=args.verbose)
 		print('loss: {}\taccuracy: {}'.format(score[0], score[1]))
