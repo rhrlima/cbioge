cc35e09ff571b2a1abc43378fad4b391078a33b7
rhrlima <ricardo_des@hotmail.com>
2019-02-12 18:35:49 -0200

    testing random search

--- algorithms/solutions.py
+++ algorithms/solutions.py
@@ -2,31 +2,42 @@ class BaseSolution:
 
 	genes = None
 	fitness = None
+	data = {}
+	evaluated = False
 
-	def __init__(self, genes, fitness=None):
+	def __init__(self, genes, fitness=None, data={}):
 		self.genes = genes
 		self.fitness = fitness
+		self.data = data
+		#self.evaluated = False
+
+	def copy(self, deep=False):
+		solution = BaseSolution(self.genes[:])
+		if deep:
+			solution.fitness = self.fitness
+			solution.data = self.data
+			solution.evaluated = self.evaluated
+		return solution
 
 	def __str__(self):
 		return str(self.genes)
 
 
-class GeneticSolution:
+class GESolution:
 
 	genotype = None
 	phenotype = None
-	fitness = None
-	data = {}
+	fitness = -1
 	evaluated = False
 
-	def __init__(self, genes, phen=None):
-		self.genotype = genes
+	def __init__(self, genotype):
+		self.genotype = genotype
 
-	def copy(self, deep=False): #shallow
-		solution = Solution(self.genotype[:])
+	def copy(self, deep=False):
+		solution = GESolution(self.genotype[:])
 		if deep:
-			solution.phenotype = self.phenotype
 			solution.fitness = self.fitness
+			solution.phenotype = self.phenotype
 			solution.evaluated = self.evaluated
 		return solution
 
