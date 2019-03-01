3aa117793a3bb7ceaeee7ab3232bfb1ff9a6a4e9
rhrlima <ricardo_des@hotmail.com>
2019-03-01 16:16:25 -0300

    updating the experience script

--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -9,9 +9,16 @@ import argparse
 import keras
 import numpy as np
 
-from algorithms import pge
+from algorithms import GrammaticalEvolution
+from algorithms import TournamentSelection
+from algorithms import OnePointCrossover
+from algorithms import PointMutation
+from algorithms import GEPrune
+from algorithms import GEDuplication
+
 from grammars import BNFGrammar
 from problems import CnnProblem
+
 from utils import checkpoint
 from keras.models import model_from_json
 
@@ -20,29 +27,30 @@ def get_arg_parsersed():
 
 	parser = argparse.ArgumentParser(prog='script.py', description='run experiment')
 
+	# not optional
 	parser.add_argument('grammar', type=str, help='grammar file in bnf format')
 	parser.add_argument('dataset', type=str, help='dataset file in pickle format')
 	
+	# checkpoint
 	parser.add_argument('-f', '--folder', dest='folder', default='checkpoints')
 	parser.add_argument('-c', '--checkpoint', default=True, type=str2bool)
 
+	# problem
 	parser.add_argument('-sd', '--seed', default=None, type=int)
-
-	parser.add_argument('-ep', '--epochs', default=5, type=int)
-	parser.add_argument('-b', '--batch', default=128, type=int)
+	parser.add_argument('-ep', '--epochs', default=1, type=int)
+	parser.add_argument('-b', '--batch', default=32, type=int)
 	parser.add_argument('-v', '--verbose', default=0, type=int)
 
-	parser.add_argument('-p', '--population', default=True, type=str2bool)
-	parser.add_argument('-e', '--evals', default=20, type=int)
-
+	# algorithm
+	parser.add_argument('-p', '--population', default=5, type=int)
+	parser.add_argument('-e', '--evals', default=10, type=int)
 	parser.add_argument('-cr', '--crossover', default=0.8, type=float)
 	parser.add_argument('-mt', '--mutation', default=0.1, type=float)
 	parser.add_argument('-pr', '--prune', default=0.1, type=float)
 	parser.add_argument('-dp', '--duplication', default=0.1, type=float)
-	parser.add_argument('-min', '--mingenes', default=2, type=int)
-	parser.add_argument('-max', '--maxgenes', default=10, type=int)
-
-	parser.add_argument('-mp', '--maxprocesses', default=8, type=int)
+	parser.add_argument('-mig', '--mingenes', default=2, type=int)
+	parser.add_argument('-mag', '--maxgenes', default=10, type=int)
+	parser.add_argument('-mp', '--maxprocesses', default=2, type=int)
 
 	return parser.parse_args()
 
@@ -61,6 +69,9 @@ if __name__ == '__main__':
 	# parses the arguments
 	args = get_arg_parsersed()
 
+	# checkpoint folder
+	checkpoint.ckpt_folder = args.folder
+
 	# read grammar and setup parser
 	parser = BNFGrammar(args.grammar)
 
@@ -69,36 +80,45 @@ if __name__ == '__main__':
 	problem.batch_size = args.batch
 	problem.epochs = args.epochs
 
-	# checkpoint folder
-	checkpoint.ckpt_folder = args.folder
+	# genetic operators to GE
+	selection = TournamentSelection(maximize=True)
+	crossover = OnePointCrossover(cross_rate=args.crossover)
+	mutation = PointMutation(mut_rate=args.mutation, min_value=0, max_value=255)
+	prune = GEPrune(prun_rate=args.prune)
+	duplication = GEDuplication(dupl_rate=args.duplication)
 
-	# changing pge default parameters
-	pge.problem = problem
-	
-	pge.SEED 		= args.seed
-	pge.POP_SIZE 	= args.population
-	pge.MAX_EVALS 	= args.evals
-	pge.CROSS_RATE 	= args.crossover
-	pge.MUT_RATE 	= args.mutation
-	pge.PRUN_RATE 	= args.prune
-	pge.DUPL_RATE 	= args.duplication
-	pge.MIN_GENES 	= args.mingenes
-	pge.MAX_GENES 	= args.maxgenes
-
-	pge.MAX_PROCESSES = args.maxprocesses
+	# changing ge default parameters
+	alg = GrammaticalEvolution(problem)
+	#alg.DEBUG = False
+	alg.SEED 		= args.seed
+	alg.POP_SIZE 	= args.population
+	alg.MAX_EVALS 	= args.evals
+	alg.MIN_GENES 	= args.mingenes
+	alg.MAX_GENES 	= args.maxgenes
+	alg.MAX_PROCESSES = args.maxprocesses
+	alg.selection 	= selection
+	alg.crossover 	= crossover
+	alg.mutation 	= mutation
+	alg.prune 		= prune
+	alg.duplication = duplication
 
 	print('--config--')
 	print('DATASET', args.dataset)
 	print('GRAMMAR', args.grammar)
+	print('EPOCHS', args.epochs)
+	print('BATCH', args.batch)
 	print('CKPT', args.folder, args.checkpoint)
 
-	print('SEED', pge.SEED)
-	print('POP', pge.POP_SIZE)
-	print('EVALS', pge.MAX_EVALS)
-	print(f'CROSS({pge.CROSS_RATE}) MUT({pge.MUT_RATE}) PRUN({pge.PRUN_RATE}) DUPL({pge.DUPL_RATE})')
+	print('SEED', args.seed)
+	print('POP', args.population)
+	print('EVALS', args.evals)
+	print(f'CROSS({args.crossover}) \
+		MUT({args.mutation}) \
+		PRUN({args.prune}) \
+		DUPL({args.duplication})')
 
 	print('--running--')
-	best = pge.execute(checkpoint=args.checkpoint)
+	best = alg.execute(checkpoint=args.checkpoint)
 
 	print('--best solution--')
 	print(best.fitness, best)
@@ -112,7 +132,7 @@ if __name__ == '__main__':
 
 		print('--training--')
 		hist = model.fit(problem.x_train, problem.y_train, 
-			batch_size=128, epochs=50, verbose=args.verbose)
+			batch_size=args.batch, epochs=args.epochs, verbose=args.verbose)
 		print('loss: {}\taccuracy: {}'.format(
 			np.mean(hist.history['loss']), np.mean(hist.history['acc'])))
 
