52d350056666baf38f4cf5626c1cfb2afbf3d3df
rhrlima <ricardo_des@hotmail.com>
2019-03-25 23:29:56 -0300

    fixing warnings in grammars' folder

--- grammars/grammar.py
+++ grammars/grammar.py
@@ -1,66 +1,64 @@
-import numpy as np
 import re
-import sys
 
 
 class BNFGrammar:
 
-	GRAMMAR = None
-	MAX_LOOPS = 1000
+    GRAMMAR = None
+    MAX_LOOPS = 1000
 
+    def __init__(self, grammar_file):
 
-	def __init__(self, grammar_file):
+        lines = []
+        with open(grammar_file, 'r') as gf:
 
-		lines = []
-		with open(grammar_file, 'r') as gf:
+            for line in gf:
 
-			for line in gf:
+                # remove spaces and '\n'
+                line = re.sub('\\s+|\n', '', line)
 
-				# remove spaces and '\n'
-				line = re.sub('\\s+|\n', '', line)
+                # if line does not match 'new rule line', append to previous
+                if re.match('<[a-z_]+>::=', line) is None:
+                    lines[len(lines)-1] += line
 
-				# if line does not match 'new rule line', append to previous
-				if re.match('<[a-z_]+>::=', line) == None:
-					lines[len(lines)-1] += line
+                elif line != '':
+                    lines.append(line)
 
-				elif line != '':
-					lines.append(line)
+        self.GRAMMAR = {'<start>': None}
+        for line in lines:
 
-		self.GRAMMAR = {'<start>': None}
-		for line in lines:
+            # split into key and productions
+            rule, prod = line.split('::=')
 
-			# split into key and productions
-			rule, prod = line.split('::=')
+            # split productions in options
+            self.GRAMMAR[rule] = prod.split('|')
 
-			# split productions in options
-			self.GRAMMAR[rule] = prod.split('|')
+            if self.GRAMMAR['<start>'] is None:
+                self.GRAMMAR['<start>'] = rule
 
-			if self.GRAMMAR['<start>'] == None: self.GRAMMAR['<start>'] = rule
+    def parse(self, codons):
+        index = 0
+        loop_count = 0
+        match = 0
 
+        prod = self.GRAMMAR['<start>']
+        while match is not None:
+            match = re.search('<[a-z_]+>', prod)
+            if match is not None:
+                token = match.group(0)
+                repl = codons[index] % len(self.GRAMMAR[token])
+                prod = prod.replace(token, self.GRAMMAR[token][repl], 1)
+                index += 1
+                if index >= len(codons):
+                    index = 0
 
-	def parse(self, codons):
-		index = 0
-		loop_count = 0
-		match = 0
+            loop_count += 1
 
-		prod = self.GRAMMAR['<start>']
-		while match != None:
-			match = re.search('<[a-z_]+>', prod)
-			if match != None:
-				token = match.group(0)
-				repl = codons[index] % len(self.GRAMMAR[token])
-				prod = prod.replace(token, self.GRAMMAR[token][repl], 1)
-				index += 1
-				if index >= len(codons): index = 0
-			
-			loop_count += 1
+            # maybe an infinite loop
+            if loop_count >= self.MAX_LOOPS:
+                return None
 
-			# maybe an infinite loop
-			if loop_count >= self.MAX_LOOPS: 
-				return None
+        prod = prod.replace('\'\'', '@')\
+            .replace('\'', '') \
+            .split('@')
 
-		prod = prod.replace('\'\'', '@')\
-			.replace('\'', '') \
-			.split('@')
-
-		return list(filter(lambda x: x != '&', prod))
+        return list(filter(lambda x: x != '&', prod))
