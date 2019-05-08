6ebad61a1b40bf1ce98d88ac99d04b0f398bb0d3
rhrlima <ricardo_des@hotmail.com>
2019-05-07 22:49:11 -0300

    fix error in script exp2 best

--- experiments/exp2_best.py
+++ experiments/exp2_best.py
@@ -2,12 +2,15 @@ import os
 
 runs = 30
 
-solutions = [
+s1 = [
     '238,135,125,101,240',
     '100,238,150,245,34,93,171,139',
     '197,23,173',
     '127,45,228',
     '168,185',
+]
+
+s2 = [
     '232,1,43,253,132,136,106,192',
     '122,220,98,97,135,106,76',
     '5,1,226,61',
@@ -16,7 +19,11 @@ solutions = [
 ]
 
 for r in range(runs):
-    for s in solutions:
-        os.system(f"./run_intel.sh {s} run_solution.py '{s} -b 128'")
+    for s in s1:
+        os.system(f"./run_intel.sh {s} run_solution.py \
+            '../grammars/cnn.bnf ../datasets/mnist.pickle {s} -b 128'")
+    for s in s2:
+        os.system(f"./run_intel.sh {s} run_solution.py \
+            '../grammars/cnn.bnf ../datasets/c10.pickle {s} -b 128'")
 
 os.system('watch qstat -n -1')
