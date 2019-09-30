1877661294afad41a4e88d9048747c370be1ef4f
rhrlima <ricardo_des@hotmail.com>
2019-09-30 16:21:34 +0200

    small changes

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,11 +1,12 @@
 import glob
 import os
+import sys
 
-import numpy
+import numpy as np
 
 import skimage.io as io
 
-def read_dataset_from_directory(path):
+def read_dataset_from_directory(path, npy=False):
 	'''expects two folders inside path:
 		dataset/
 		--image/
@@ -16,7 +17,17 @@ def read_dataset_from_directory(path):
 
 	for img, msk in zip(images, labels):
 
-		img = io.imread(img)
-		msk = io.imread(msk)
+		img = np.load(img) if npy else io.imread(img)
+		msk = np.load(msk) if npy else io.imread(msk)
 
-		print(img.shape, msk.shape)
+		print(img.shape, img.min(), img.max(), msk.shape, msk.min(), msk.max())
+
+
+if __name__ == '__main__':
+
+	if len(sys.argv) == 2:
+		read_dataset_from_directory(sys.argv[1])
+	elif len(sys.argv) == 3:
+		read_dataset_from_directory(sys.argv[1], sys.argv[2])
+	else:
+		print('expected: script.py <path> [True|False]')
