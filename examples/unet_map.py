f45710a2a432eb4200a21013b74464e412f69e54
rhrlima <ricardo_des@hotmail.com>
2019-10-11 17:06:13 +0200

    unet grammar implementation

--- examples/unet_map.py
+++ examples/unet_map.py
@@ -4,10 +4,15 @@ from grammars import BNFGrammar
 
 if __name__ == '__main__':
 
-	parser = BNFGrammar('grammars/unet.bnf')
-	problem = UNetProblem(parser, None)
+	dset = {
+		'input_shape': (8, 8, 1)
+	}
 
+	parser = BNFGrammar('grammars/unet.bnf')
+	problem = UNetProblem(parser, dset)
+
+	#for _ in range(10):
 	gen = parser.dsge_create_solution()
-	fen = problem.map_genotype_to_phenotype(gen)
-	#print(gen)
-	#print(fen)
+	print(gen)
+	fen = parser.dsge_recursive_parse(gen)
+	problem._map_genotype_to_phenotype(gen)
--- problems/__init__.py
+++ problems/__init__.py
@@ -1,14 +1,14 @@
-from problems.problem import CNNProblem
+from problems.classification import CNNProblem
+from problems.segmentation import UNetProblem
 
 from problems.regression import SymbolicRegressionProblem
-
 from problems.stringmatch import StringMatchProblem
-from problems.segmentation import UNetProblem
 
 
 __all__ = [
     'CNNProblem',
+    'UNetProblem',
+
     'SymbolicRegressionProblem',
     'StringMatchProblem',
-    'UNetProblem'
 ]
