4b37ffe2fdf8e93b9e3bd030c7abb403b80507f3
rhrlima <ricardo_des@hotmail.com>
2019-11-13 17:00:54 +0100

    experiment without generator

--- experiments/unet_experiment.py
+++ experiments/unet_experiment.py
@@ -18,12 +18,12 @@ def get_args():
 
     args.add_argument('dataset', type=str) #dataset
 
-    args.add_argument('-trs', '--train', type=int, default=5) #train steps
-    args.add_argument('-tes', '--test', type=int, default=5) #test steos
-    args.add_argument('-aug', '--augment', type=int, default=0) #augmentation
+    args.add_argument('-tr', '--train', type=int, default=5) #train steps
+    args.add_argument('-te', '--test', type=int, default=5) #test steos
+
     args.add_argument('-p', '--predict', type=int, default=0) #predict
     args.add_argument('-b', '--batch', type=int, default=1) #batch
-    args.add_argument('-s', '--shuffle', type=int, default=0) #shuffle
+
     args.add_argument('-v', '--verbose', type=int, default=1) #verbose
 
     args.add_argument('-w', '--workers', type=int, default=1) #workers    
@@ -37,15 +37,8 @@ def run():
 
     print(args)
 
-    dset_args = json.loads(open(args.dataset, 'r').read())
-    dset_args['train_steps'] = args.train
-    dset_args['test_steps'] = args.test
-
-    data_aug = ImageDataGenerator(**dset_args['aug']) if args.augment > 0 else None
-    train_gen = DataGenerator(dset_args['train_path'], dset_args['input_shape'], batch_size=args.batch, data_aug=data_aug, shuffle=args.shuffle)
-    test_gen = DataGenerator(dset_args['test_path'], dset_args['input_shape'], batch_size=args.batch, shuffle=args.shuffle)
-
-    problem = UNetProblem(None, dset_args, train_gen, test_gen)
+    problem = UNetProblem(None)
+    problem.read_dataset_from_pickle(args.dataset)
     problem.verbose = args.verbose
     problem.workers = args.workers
     problem.multiprocessing = args.multip
@@ -53,7 +46,7 @@ def run():
     solution = GESolution([])
     solution.phenotype = unet(dset_args['input_shape']).to_json()
 
-    result = problem.evaluate(solution.phenotype, predict=args.predict)
+    result = problem.evaluate2(solution.phenotype)
     print(result)
 
 
