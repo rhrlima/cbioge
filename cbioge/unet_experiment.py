04c718d2b74c8c9fb16e96ed8cc9329dfeb20f8a
rhrlima <ricardo_des@hotmail.com>
2022-01-25 17:25:28 -0300

    test unet exp with class weights

--- cbioge/unet_experiment.py
+++ cbioge/unet_experiment.py
@@ -6,11 +6,11 @@ DEFAULTS = {
     'grammar': 'cbioge/assets/tmp_ignore/unet_restricted.json',
     'dataset': 'cbioge/assets/datasets/bsds500norm-2022.pickle',
 
-    'epochs': 10,
+    'epochs': 50,
     'batch': 10,
 
     'pop': 10,
-    'evals': 20,
+    'evals': 100,
 }
 
 
@@ -18,21 +18,27 @@ def run_experiment(args):
 
     import keras
     import cbioge
+    from sklearn.utils import class_weight
 
     metric = cbioge.problems.dnns.image_metrics.WeightedMetric(
-        w_dic=0.4,
+        w_dic=0.6,
         w_jac=0.1,
-        w_sen=0.4,
+        w_sen=0.2,
         w_spe=0.1,
     )
     accuracy = metric.get_metric()
 
+    dataset = cbioge.datasets.Dataset.from_pickle(args.dataset,
+        train_size=args.train_size,
+        valid_size=args.valid_size,
+        test_size=args.test_size)
+
+    _, y_train = dataset.get_data('train')
+    y_shape = (y_train.shape[0] * y_train.shape[1] * y_train.shape[2])
+
     problem = cbioge.problems.UNetProblem(
         cbioge.grammars.Grammar(args.grammar),
-        cbioge.datasets.Dataset.from_pickle(args.dataset,
-            train_size=args.train_size,
-            valid_size=args.valid_size,
-            test_size=args.test_size),
+        dataset,
         batch_size=args.batch,
         epochs=args.epochs,
         opt=keras.optimizers.Adam(lr=1e-4),
@@ -40,6 +46,11 @@ def run_experiment(args):
         train_args= {
             'workers': 4,
             'use_multiprocessing': True,
+            'class_weight': class_weight.compute_class_weight(
+                'balanced',
+                classes=[0, 1],
+                y=y_train.reshape(y_shape)
+            ),
         })
 
     cbioge.algorithms.GrammaticalEvolution(problem,
